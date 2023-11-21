fc596f3e64882ad4bf9afe92704ccd250844054b
markerpen92 <markchou920219@gmail.com>
2023-11-22 01:18:09 +0800

    20231122

--- CoCaptureEffect/DeCompiler.py
+++ CoCaptureEffect/DeCompiler.py
@@ -1,7 +1,4 @@
-Record = []
-
 def binary_to_utf8(binary_str):
-    # 忽略換行符
     if binary_str == '\n':
         return ''
 
@@ -14,18 +11,14 @@ def ReadRecord(readfilename, Writefilename):
         with open(readfilename, 'r') as file:
             for line in file:
                 utf8_result = ''.join(chr(int(line[i:i+8], 2)) for i in range(0, len(line)-1, 8))
-                Record.append(utf8_result)
-
-        result_string = '\n'.join(Record)
-
-        append_string_to_file(result_string, Writefilename)
+                append_string_to_file(utf8_result , Writefilename)
 
     except Exception as e:
         print(f"Error: {e}")
 
 def append_string_to_file(input_string, filename):
     try:
-        with open(filename, 'w') as file:
+        with open(filename, 'a') as file:
             file.write(input_string + '\n')
         print(f"Input into file: {filename}'s End")
     except Exception as e:
